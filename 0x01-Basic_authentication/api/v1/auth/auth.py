06c6affb9d9230d8d0322119e0e481fd93a8099b
Serinajefthas <serina.jefthas@gmail.com>
2024-02-13 23:06:26 +0000

    task 4 method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,11 +10,20 @@ class Auth():
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ returns bool of whether path is in list execluded_paths or not """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+        path = path.rstrip('/')
+        excluded_paths = [p.rstrip('/') for p in excluded_paths]
+
+        return path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
         """returns authorization header"""
-        return None
+        if request is None or "Authorization" not in request.headers:
+            return None
+        return request.headers["Authorization"]
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ returns current user"""
