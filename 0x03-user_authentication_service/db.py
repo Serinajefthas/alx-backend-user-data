419d7e95eea5408cbb07bcefb82162ff470f322b
Serinajefthas <serina.jefthas@gmail.com>
2024-02-22 16:24:06 +0000

    add update user task 3

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 """DB module
 """
+import logging
+from typing import Dict
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
-from user import Base
+from user import Base, User
+
+logging.disable(logging.WARNING)
 
 
 class DB:
@@ -31,19 +36,44 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """returns User object and adds user to db"""
         new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
+        try:
+            self._session.add(new_user)
+            self._session.commit()
+        except Exception as e:
+            print(f"Error adding user to database: {e}")
+            self._session.rollback()
+            raise
         return new_user
 
-    def find_user_by(self, **kwargs):
+    def find_user_by(self, **kwargs: Dict[str, str]) -> User:
         """find user by filtering user tbl by input args"""
+        session = self._session
         try:
-            user = self._session.query(User).filter_by(**kwargs).first()
+            user = session.query(User).filter_by(**kwargs).one()
             if user is None:
                 raise NoResultFound("Not found")
             return user
         except InvalidRequestError as e:
             raise InvalidRequestError("Invalid") from e
+
+    def update_user(self, user_id: int, **kwargs: dict[str, str]) -> None:
+        """updates user's attributes w user id and arbitary keywords"""
+        try:
+            user = self.find_user_by(id=user_id)
+        except NoResultFound:
+            raise NoResultFound(f"No user found with id {user_id}")
+
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                setattr(user, key, value)
+            else:
+                raise ValueError(f"Invalid attribute '{key}'")
+
+        """saves to db"""
+        try:
+            self._session.commit()
+        except InvalidRequestError:
+            raise InvalidRequestError("Invalid request")
--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """Creates user model"""
+
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -11,7 +13,7 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    email = Column(Stringi(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
